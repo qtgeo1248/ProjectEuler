951bd97e47c4737703c6016bb90ebda31c32ba7e
qtgeo1248 <gzhou7819@gmail.com>
2020-12-23 18:32:09 -0500

    Finished P7

---
+++ P7/ans.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+
+int main() {
+    char *filename = "num.txt";
+    FILE *f = fopen(filename, "r");
+    char *line = NULL; size_t len = 0; char *thing = NULL;
+    getline(&line, &len, f);
+    size_t max = strtol(line, &thing, 10);
+
+    size_t *primes = calloc(sizeof(size_t), max);
+    size_t num = 0;
+    for (size_t test = 2; num < max; test++) {
+        bool isPrime = true;
+        for (size_t i = 0; i < num; i++)
+            if (test % primes[i] == 0)
+                isPrime = false;
+        if (isPrime) {
+            primes[num] = test;
+            num++;
+        }
+    }
+    printf("Answer: %zu\n", primes[max - 1]);
+    return 0;
+}
